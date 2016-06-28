9faa5864d55a53cf0aab6696a1b35b9c0fbc0965
Nandhitha R <NandhithaR@users.noreply.github.com>
2016-06-28 12:46:52 +0530

    Update core.clj

--- src/cumulus/core.clj
+++ src/cumulus/core.clj
@@ -12,7 +12,6 @@
    :jdbc-url   jdbc-url
    :test-query test-query})
 
-
 (defn jdbc-params
   "Given database-specific parameters, return a map of fundamental JDBC params consisting of
   the following mandatory keys (all of them having string values):
@@ -20,7 +19,24 @@
   :jdbc-url
   :test-query"
   [db-type db-params]
-  (let [R (partial i/reqd db-params)]
+  
+  (when-let [key (get db-params :port)]
+    (i/parse_fn db-params :port))
+  
+  (when-let [key (get db-params :database)]
+   (i/typeCheck-string db-params :database))
+  
+  (when-let [key (get db-params :host)]
+    (i/typeCheck-string db-params :host))
+  
+  (let [R (partial i/reqd db-params)
+        P (fn ([k] (get db-params k))
+            ([k d] (get db-params k d)))
+        Q (fn ([k] (if-let [v (P k)] 
+                     (str ":" v)
+                     ""))
+            ([k d] (str ":" (P k d))))]
+    
     (case db-type
       ;;embedded
       :odbc            (raw-params
@@ -28,9 +44,12 @@
                          (format "jdbc:odbc:%s"  (R :dsn))
                          nil)
       
-      :axiondb         (raw-params
+      :axiondb         (raw-params 
                          "org.axiondb.jdbc.AxionDriver"
-                         (format "jdbc:axiondb:%s"  (R :database))
+                         (let [target (:target db-params)]
+                           (case target
+                             :memory (format "jdbc:axiondb:%s"  (R :database))
+                             :filesys (format "jdbc:axiondb:%s:%s" (R :database) (R :db-path))))
                          "SELECT 1")
       
       :derby           (raw-params 
@@ -41,17 +60,17 @@
                              :filesys   (format "jdbc:derby:directory:%s;create=true;" (R :database))
                              :classpath (format "jdbc:derby:classpath:%s"              (R :database))
                              :jar       (format "jdbc:derby:jar:(%s)%s"                (R :jar-path) (R :database))
-                             :network   (format "jdbc:derby://%s%s/%s;create=true;"    (R :host) (R :port) (R :database))
+                             :network   (format "jdbc:derby://%s%s/%s;create=true;"    (R :host) (Q :port) (R :database))
                              (i/expected ":target to be :memory, :filesys, :classpath, :jar or :network" target)))
                          "values(1)")
       
-      :h2              (raw-params 
+      :h2              (raw-params
                          "org.h2.Driver"
                          (let [target (:target db-params)]
                            (case target 
                              :memory  (format "jdbc:h2:mem:%s"             (R :database))
                              :filesys (format "jdbc:h2:file:%s"            (R :database))
-                             :network (format "jdbc:h2:tcp:%s%s/%s"        (R :host) (R :port) (R :database))
+                             :network (format "jdbc:h2:tcp:%s%s/%s"        (R :host) (Q :port) (R :database))
                              (i/expected ":target to be :memory, :filesys or :network" target)))
                          "SELECT 1")
       
@@ -61,7 +80,7 @@
                            (case target
                              :memory  (format "jdbc:hsqldb:mem:%s"         (R :database))
                              :filesys (format "jdbc:hsqldb:file:%s"        (R :database))
-                             :network (format "jdbc:hsqldb:hsql://%s%s/%s" (R :host) (R :port) (R :database))
+                             :network (format "jdbc:hsqldb:hsql://%s%s/%s" (R :host) (Q :port) (R :database))
                              (i/expected ":target to be :memory, :filesys or :network" target)))
                          "SELECT 1 FROM INFORMATION_SCHEMA.SYSTEM_USERS")
       
@@ -82,53 +101,53 @@
       ;; network OSS
       :cubrid          (raw-params 
                          "cubrid.jdbc.driver.CUBRIDDriver"
-                         (format "jdbc:cubrid:%s:%s:%s"                 (R :host) (R :port) (R :database))
+                         (format "jdbc:cubrid:%s%s:%s"                 (R :host) (Q :port) (R :database))
                          "SELECT 1;")
       
       :firebird        (raw-params
                          "org.firebirdsql.jdbc.FBDriver"
-                         (format "jdbc:firebirdsql://%s:%s/%s"          (R :host) (R :port) (R :database))
+                         (format "jdbc:firebirdsql://%s%s/%s"          (R :host) (Q :port) (R :database))
                          "SELECT CAST(1 AS INTEGER) FROM rdb$database;")
       
       :jtds-sqlserver  (raw-params
                          "net.sourceforge.jtds.jdbc.Driver"
-                         (format "jdbc:jtds:sqlserver://%s:%s%s"        (R :host) (R :port) (R :database))
+                         (format "jdbc:jtds:sqlserver://%s%s%s"        (R :host) (Q :port) (R :database))
                          "select 1;")
       
       :jtds-sybase     (raw-params 
                          "net.sourceforge.jtds.jdbc.Driver"
-                         (format "jdbc:jtds:sybase://%s:%s%s"           (R :host) (R :port) (R :database))
+                         (format "jdbc:jtds:sybase://%s%s%s"           (R :host) (Q :port) (R :database))
                          "select 1;")
       
       :monetdb         (raw-params
                          "nl.cwi.monetdb.jdbc.MonetDriver"
-                         (format "jdbc:monetdb://%s:%s/%s"              (R :host) (R :port) (R :database))
+                         (format "jdbc:monetdb://%s%s/%s"              (R :host) (Q :port) (R :database))
                          "SELECT 1;")
       
       :mysql           (raw-params 
                          "com.mysql.jdbc.Driver"
-                         (format "jdbc:mysql://%s:%s/%s"                (R :host) (R :port) (R :database))
+                         (format "jdbc:mysql://%s%s/%s"                (R :host) (Q :port) (R :database))
                          "SELECT 1;")
       
       :postgresql      (raw-params 
                          "org.postgresql.Driver"
-                         (format "jdbc:postgresql://%s:%s/%s"           (R :host) (R :port) (R :database))
+                         (format "jdbc:postgresql://%s%s/%s"           (R :host) (Q :port) (R :database))
                          "SELECT version();")
       
       ;; network proprietary
       :db2             (raw-params 
                          "com.ibm.db2.jcc.DB2Driver"
-                         (format "jdbc:db2://%s:%s/%s"                  (R :host) (R :port) (R :database))
+                         (format "jdbc:db2://%s%s/%s"                  (R :host) (Q :port) (R :database))
                          "select * from sysibm.SYSDUMMY1;")
       
       :oracle          (raw-params
                          "oracle.jdbc.driver.OracleDriver"
                          (let [target (:target db-params)]
                            (case target
-                             :system-id    (format "jdbc:oracle:thin:@%s:%s:%s"        (R :host) (R :port) (R :database))
-                             :service-name (format "jdbc:oracle:thin:@//%s:%s/%s"      (R :host) (R :port) (R :database))
+                             :system-id    (format "jdbc:oracle:thin:@%s%s:%s"        (R :host) (Q :port) (R :database))
+                             :service-name (format "jdbc:oracle:thin:@//%s%s/%s"      (R :host) (Q :port) (R :database))
                              :tns-name     (format "jdbc:oracle:thin:@%s"              (R :database))
-                             :ldap         (format "jdbc:oracle:thin:@ldap://%s/%s:%s" (R :host) (R :port) (R :database))
+                             :ldap         (format "jdbc:oracle:thin:@ldap://%s/%s:%s" (R :host) (Q :port) (R :database))
                              :oci          (format "jdbc:oracle:oci:@%s"               (R :database))
                              :oci8         (format "jdbc:oracle:oci8:@%s"              (R :database))
                              (i/expected ":target to be :system-id, :service-name, :tns-name, :ldap,oci or :oci8" target)))
@@ -136,17 +155,17 @@
       
       :sapdb           (raw-params
                          "com.sap.dbtech.jdbc.DriverSapDB"
-                         (format "jdbc:sapdb://%s:%s/%s"                 (R :host) (R :port) (R :database))
+                         (format "jdbc:sapdb://%s%s/%s"                 (R :host) (Q :port) (R :database))
                          "SELECT 1 FROM DUAL")
       
       :sqlserver       (raw-params
                          "com.microsoft.sqlserver.jdbc.SQLServerDriver"
-                         (format "jdbc:sqlserver://%s\\%s:%s"            (R :host) (R :instance) (R :port))
+                         (format "jdbc:sqlserver://%s%s%s"            (Q :host) (Q :instance) (Q :port))
                          "SELECT 1")
       
       :sybase          (raw-params
                          "com.sybase.jdbc2.jdbc.SybDriver"
-                         (format "jdbc:sybase:Tds:%s:%s?ServiceName=%s"  (R :host) (R :port) (R :database))
+                         (format "jdbc:sybase:Tds:%s:%sServiceName=%s?"  (R :host) (Q :port) (Q :database))
                          "SELECT 1")
       
       (throw (IllegalArgumentException.
